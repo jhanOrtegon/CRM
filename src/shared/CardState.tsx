450756c716b8cf2e2644fbf8e75f60ef86a392b1
jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2023-08-08 09:10:12 -0500

    lunes changes

--- src/shared/CardState.tsx
+++ src/shared/CardState.tsx
@@ -1,3 +1,5 @@
+import { Box } from "@mui/material"
+
 type TCardState = {
     type: 'success' | 'info' | 'error' | 'gray' | 'warning'
     text: string,
@@ -6,9 +8,9 @@ type TCardState = {
 
 export const CardState = ({ type='success', text }: TCardState) => (
     // <div className={`-bg--daisy-state${variant === 'full' ? '-full' : ''}-${type} ${variant === 'full' ? 'text-white' : `-text--daisy-state-full-${type}`} p-1 m-0 w-full rounded-xl`}>
-    <div className={`${type === 'success' ? '-bg--daisy-state-success -text--daisy-state-full-success' : '-bg--daisy-state-error -text--daisy-state-full-error'} p-2 font-bold m-0 w-full rounded-xl`}>
+    <Box sx={{padding:'5px'}} className={`${type === 'success' ? '-bg--daisy-state-success -text--daisy-state-full-success' : '-bg--daisy-state-error -text--daisy-state-full-error'} font-bold m-0 w-full rounded-xl`}>
         {text}
-    </div>
+    </Box>
 )
 
 
