2054f7edd97a9882a43fbb04f14e318199a71b64
jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2023-08-08 08:46:28 -0500

    new progress

--- src/shared/CardState.tsx
+++ src/shared/CardState.tsx
@@ -4,8 +4,9 @@ type TCardState = {
     variant?: 'full'
 }
 
-export const CardState = ({ type, text, variant }: TCardState) => (
-    <div className={`-bg--daisy-state${variant === 'full' ? '-full' : ''}-${type} ${variant === 'full' ? 'text-white' : `-text--daisy-state-full-${type}`} p-1 m-0 w-full rounded-xl`}>
+export const CardState = ({ type='success', text, variant }: TCardState) => (
+    // <div className={`-bg--daisy-state${variant === 'full' ? '-full' : ''}-${type} ${variant === 'full' ? 'text-white' : `-text--daisy-state-full-${type}`} p-1 m-0 w-full rounded-xl`}>
+    <div className={`${type === 'success' ? '-bg--daisy-state-success -text--daisy-state-full-success' : '-bg--daisy-state-error -text--daisy-state-full-error'} p-2 font-bold m-0 w-full rounded-xl`}>
         {text}
     </div>
 )
